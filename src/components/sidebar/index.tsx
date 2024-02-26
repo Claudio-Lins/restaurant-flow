190138626196677ef4c4c2b6db832a8fb8893d14
Claudio Lins <claudio.lins@me.com>
2024-02-26 08:54:51 +0000

    update: Sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,11 +1,19 @@
+import { currentUser } from "@/lib/auth";
+
+// import { ExtendedUser } from "../../../../next-auth";
 import { Separator } from "../ui/separator";
 import { AdminSvg } from "./admin";
 import { Navigation } from "./navigation";
 
-export function Sidebar() {
+export async function Sidebar() {
+  const user = await currentUser();
+  // const extendedUser = user as ExtendedUser | undefined;
   return (
     <aside className="space-y-6 border-r border-muted-foreground px-5 py-8">
-      <AdminSvg />
+      <div className="flex items-center gap-2">
+        <AdminSvg />
+        <h3 className="mb-0 inline-block text-muted">{user?.name}</h3>
+      </div>
       <Separator />
       <Navigation />
     </aside>
